6e74e303028a2a852b18d96739e7b348adaaf218
alex <cyy0523xc@gmail.com>
2020-03-18 21:14:22 +0800

    add zip.sh

---
+++ zip.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# 
+# 
+# Author: alex
+# Created Time: 2020年03月18日 星期三 21时12分01秒
+cd ../
+name="python-image-utils"
+if [ ! -d "$name" ]; then
+    echo "$PWD: 当前目录错误."
+fi
+version=
+if [ $# = 1 ]; then 
+    version="-$1"
+fi
+
+date_str=`date -I`
+filename="$name-${date_str//-/}$version".zip
+if [ -f "$filename" ]; then
+    rm -f "$filename"
+fi
+
+# 第一次打包时，记得把模型文件也打包在一起
+# 正式发布时，记得过滤src目录中的内容
+# -x "*/src/*" \
+zip -r "$filename" "$name" \
+    -x "*/.git/*" \
+    -x "*/.*" \
+    -x "*/*/*.swp" \
+    -x "*/__pycache__/*" \
+    -x "*/models/*/*.weights" \
+    -x "*/models/*/*/*.weights" \
+    -x "*/zip.sh" 
+
+scp "$filename" ibbd@192.168.80.242:~/ocr/ocr-v3/
+
+date
