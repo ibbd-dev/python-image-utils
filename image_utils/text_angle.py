998df0016474c80814206934966be78557a093ca
alex <cyy0523xc@gmail.com>
2020-03-21 14:31:31 +0800

    修改函数注释

--- image_utils/text_angle.py
+++ image_utils/text_angle.py
@@ -4,15 +4,14 @@
 # Author: alex
 # Created Time: 2020年01月03日 星期五 18时36分08秒
 import cv2
-import math
 import numpy as np
 from scipy.ndimage import filters, interpolation
 
 
-def estimate_skew_angle(raw, fine_tune_num=3, step_start=1.5):
+def estimate_skew_angle(gray, fine_tune_num=3, step_start=1.5):
     """
     估计图像文字角度
-    :param raw 待纠正的图像
+    :param gray 待纠正的灰度图像
     :param fine_tune_num 微调的次数, 界定了微调的精度
         当该值为n时，表示微调角度精确到step_start乘以10的-(n-1)次方
     :param step_start 步长的初始值
@@ -26,8 +25,8 @@ def estimate_skew_angle(raw, fine_tune_num=3, step_start=1.5):
             f = float(max_scale)/max(im.shape[0], im.shape[1])
         return cv2.resize(im, (0, 0), fx=f, fy=f)
 
-    raw = resize_im(raw, scale=600, max_scale=900)
-    image = raw-np.amin(raw)
+    gray = resize_im(gray, scale=600, max_scale=900)
+    image = gray-np.amin(gray)
     image = image/np.amax(image)
     m = interpolation.zoom(image, 0.5)
     m = filters.percentile_filter(m, 80, size=(20, 2))
@@ -66,44 +65,11 @@ def fine_tune_angle(image, step, start=0):
     return angle
 
 
-def estimate_skew_angle2(srcImage):
-    """利用直线了纠正角度"""
-    midImage = cv2.cvtColor(srcImage, cv2.COLOR_BGR2GRAY)
-    dstImage = cv2.Canny(midImage, 50, 200, 3)
-
-    # 通过霍夫变换检测直线
-    # 第4个参数就是阈值，阈值越大，检测精度越高
-    # 由于图像不同，阈值不好设定，因为阈值设定过高导致无法检测直线，阈值过低直线太多，速度很慢
-    # 霍夫变换
-    lines = cv2.HoughLines(dstImage, 1, np.pi / 180, 0)
-    rotate_angle = 0
-    for rho, theta in lines[0]:
-        a = np.cos(theta)
-        b = np.sin(theta)
-        x0 = a * rho
-        y0 = b * rho
-        x1 = int(x0 + 1000 * (-b))
-        y1 = int(y0 + 1000 * (a))
-        x2 = int(x0 - 1000 * (-b))
-        y2 = int(y0 - 1000 * (a))
-        if x1 == x2 or y1 == y2:
-            continue
-        t = float(y2 - y1) / (x2 - x1)
-        rotate_angle = math.degrees(math.atan(t))
-        if rotate_angle > 45:
-            rotate_angle = -90 + rotate_angle
-        elif rotate_angle < -45:
-            rotate_angle = 90 + rotate_angle
-
-    return math.radians(rotate_angle)
-    # return rotate_angle
-
-
 if __name__ == '__main__':
     import sys
     from convert import rotate
-    img = cv2.imread(sys.argv[1])
-    angle = estimate_skew_angle2(img)
+    img = cv2.imread(sys.argv[1], cv2.COLOR_BGR2GRAY)
+    angle = estimate_skew_angle(img)
     print(angle)
     new_img = rotate(img, angle)
     cv2.imwrite(sys.argv[2], new_img)
