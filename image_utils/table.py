6fb8ac3d14db2ec3b00227113e462a904dde716a
alex <cyy0523xc@gmail.com>
2020-03-21 11:33:47 +0800

    增加检测表格倾斜角度

--- image_utils/table.py
+++ image_utils/table.py
@@ -5,10 +5,28 @@
 # Created Time: 2020年03月20日 星期五 16时41分48秒
 import cv2
 import numpy as np
+from math import atan
 from sklearn.cluster import DBSCAN
 
 
+def detect_table_angle(gray, scale):
+    """检测表格倾斜角度
+    :param gray 灰度图
+    :param scale 检测参数
+    :return 倾斜弧度，如果需要转换为角度: math.degrees
+    """
+    binary = cv2.adaptiveThreshold(~gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                   cv2.THRESH_BINARY, 15, -10)
+    row_img = detect_row_line(binary, scale)
+    n, lines = cluster_fit_lines(row_img)
+    if n < 3:
+        return None
+    angles = [line[0] for line in lines]
+    return atan(sum(angles)/len(angles))
+
+
 def detect_line(gray, scale):
+    """检测横线和竖线"""
     binary = cv2.adaptiveThreshold(~gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                                    cv2.THRESH_BINARY, 15, -10)
     row_img = detect_row_line(binary, scale)
@@ -17,40 +35,50 @@ def detect_line(gray, scale):
 
 
 def detect_col_line(binary, scale):
+    """检测竖线"""
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, scale))
     eroded = cv2.erode(binary, kernel, iterations=1)
     return cv2.dilate(eroded, kernel, iterations=1)
 
 
 def detect_row_line(binary, scale):
+    """检测横线"""
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (scale, 1))
     eroded = cv2.erode(binary, kernel, iterations=1)
     return cv2.dilate(eroded, kernel, iterations=1)
 
 
-def cluster_lines(data):
-    db = DBSCAN(eps=3, min_samples=2, metric='manhattan', algorithm='auto').\
-        fit(data)
+def cluster_lines(data, eps=3, min_samples=2, metric='manhattan'):
+    """线条聚类"""
+    db = DBSCAN(eps=eps, min_samples=min_samples, metric=metric).fit(data)
     labels = db.labels_
-    # n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_clusters_ = max(labels) + 1
     return n_clusters_, labels
 
 
 def fit_line(points):
+    """拟合直线
+    :params points [[y, x]] 图像坐标：[y, x]
+    :return [a, b] 直线参数：y=ax+b
+    """
     X = points[:, 1]
     Y = points[:, 0]
-    params = np.polyfit(X, Y, 1)
-    return params
+    line = np.polyfit(X, Y, 1)
+    return line
 
 
-def cluster_fit_lines(row_img):
-    idx_row = np.argwhere(row_img == 255)
-    n, labels = cluster_lines(idx_row)
+def cluster_fit_lines(lines_img):
+    """直线聚合并拟合直线
+    :param lines_img 直线的黑白图像
+    :return n int 直线数量
+    :return lines [[a, b]] 直线方程的参数
+    """
+    points_idx = np.argwhere(lines_img == 255)
+    n, labels = cluster_lines(points_idx)
     lines = []
     for i in range(n):
-        points = idx_row[labels == i]
-        line = fit_line(points)
+        line_points_idx = points_idx[labels == i]
+        line = fit_line(line_points_idx)
         lines.append(line)
 
     return n, lines
--- image_utils/text_angle.py
+++ image_utils/text_angle.py
@@ -50,47 +50,6 @@ def estimate_skew_angle(raw, fine_tune_num=3, step_start=1.5):
     return angle
 
 
-def estimate_skew_angle3(raw, fine_tune_num=3, step_start=1.5):
-    """
-    估计图像文字角度
-    :param raw 待纠正的图像
-    :param fine_tune_num 微调的次数, 界定了微调的精度
-        当该值为n时，表示微调角度精确到step_start乘以10的-(n-1)次方
-    :param step_start 步长的初始值
-        当该值为a时，其纠正的角度范围是[-10*a, 10*a]。该值不应该大于4.5
-    :return angle 需要纠正的角度
-    """
-    def resize_im(im, scale, max_scale=None):
-        f = float(scale)/min(im.shape[0], im.shape[1])
-        if max_scale is not None and \
-                f*max(im.shape[0], im.shape[1]) > max_scale:
-            f = float(max_scale)/max(im.shape[0], im.shape[1])
-        return cv2.resize(im, (0, 0), fx=f, fy=f)
-
-    raw = resize_im(raw, scale=600, max_scale=900)
-    image = raw-np.amin(raw)
-    image = image/np.amax(image)
-    m = interpolation.zoom(image, 0.5)
-    m = filters.percentile_filter(m, 80, size=(20, 2))
-    m = filters.percentile_filter(m, 80, size=(2, 20))
-    m = interpolation.zoom(m, 1.0/0.5)
-
-    w, h = min(image.shape[1], m.shape[1]), min(image.shape[0], m.shape[0])
-    flat = np.clip(image[:h, :w]-m[:h, :w]+1, 0, 1)
-    d0, d1 = flat.shape
-    o0, o1 = int(0.1*d0), int(0.1*d1)
-    flat = np.amax(flat)-flat
-    flat -= np.amin(flat)
-    est = flat[o0:d0-o0, o1:d1-o1]
-
-    angle, step = 0, step_start   # 纠正角度的初始值和步长
-    for _ in range(fine_tune_num):
-        angle = fine_tune_angle(est, step, start=angle)
-        step /= 10
-
-    return angle
-
-
 def fine_tune_angle(image, step, start=0):
     """微调纠正
     在某个角度start的周围进行微调
@@ -108,6 +67,7 @@ def fine_tune_angle(image, step, start=0):
 
 
 def estimate_skew_angle2(srcImage):
+    """利用直线了纠正角度"""
     midImage = cv2.cvtColor(srcImage, cv2.COLOR_BGR2GRAY)
     dstImage = cv2.Canny(midImage, 50, 200, 3)
 
