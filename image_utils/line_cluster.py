e423086cd82e549e465bf10c2f2d6b3ed27ef82d
alex <cyy0523xc@gmail.com>
2020-03-27 09:57:48 +0800

    fix line cluster: 待测试

--- image_utils/line_cluster.py
+++ image_utils/line_cluster.py
@@ -7,16 +7,21 @@ import numpy as np
 from ibbd_algo.optics import Optics
 
 
-def line_cluster(lines, enpoints,
+def line_cluster(lines, line_types, enpoints,
                  max_radius=3, min_samples=2, cluster_thr=2):
-    """线段聚类
-    :param lines list 线段方程，y=ax+b: [(a, b)]
+    """将有交点的线段聚合在一起
+    线段方程类型为:
+        True: y=ax+b
+        False: x=ay+b
+    :param lines list 线段方程参数[(a, b)]
+    :param line_types list 线段方程的类型，跟lines参数对应，取值True or False
     :param enpoints list 线段的端点，注意每个线段有两个端点: [[(y1, x1), (y2, x2)]]
     :param min_samples, max_radius: 聚类参数
     :return labels
     """
-    data = [(a, b, x1, y1, x2, y2)
-            for ((a, b), ((x1, y1), (x2, y2))) in zip(lines, enpoints)]
+    data = [(l_type, a, b, x1, y1, x2, y2)
+            for ((a, b), l_type, ((x1, y1), (x2, y2))) in \
+            zip(lines, line_types, enpoints)]
     optics = Optics(max_radius, min_samples, distance=distance)
     optics.fit(data)
     return optics.cluster(cluster_thr)
@@ -26,28 +31,51 @@ def distance(line1, line2):
     """计算两个线段的距离
     :param line1,line2: [a, b, x1, y1, x2, y2]
     """
-    a1, b1, x11, y11, x12, y12 = line1.data
-    a2, b2, x21, y21, x22, y22 = line2.data
-    if abs(a1-a2) < 0.001:     # 平行
-        return Optics.inf
+    l_type1, a1, b1, x11, y11, x12, y12 = line1.data
+    l_type2, a2, b2, x21, y21, x22, y22 = line2.data
+
+    def format_fraction(val):
+        c_val = 0.000000001
+        if -c_val < val < 0:
+            val = -c_val
+        elif c_val > val >= 0:
+            val = c_val
+
+        return val
+
     # 计算直线交点
-    x0 = (b2-b1) / (a1-a2)
-    y0 = a1*x0 + b1
+    if l_type1 and l_type2:
+        x0 = (b2-b1) / format_fraction(a1-a2)
+        y0 = a1*x0 + b1
+    elif not l_type1 and not l_type2:
+        y0 = (b2-b1) / format_fraction(a1-a2)
+        x0 = a1*y0 + b1
+    elif l_type1 and not l_type2:
+        # y=a1*x+b1 and x=a2*y+b2
+        y0 = (a1*b2+b1) / format_fraction(1-a1*a2)
+        x0 = a2*y0 + b2
+    elif not l_type1 and l_type2:
+        # x=a1*y+b1 and y=a2*x+b2
+        x0 = (a1*b2+b1) / format_fraction(1-a1*a2)
+        y0 = a2*x0 + b2
+
+    def point_line_dist(x1, y1, x2, y2):
+        """计算点到线的距离"""
+        if x1 <= x0 <= x2 and y1 <= y0 <= y2:
+            dist = 0
+        else:
+            # 到两端点的最小距离
+            dist = min(np.linalg.norm([x0-x1, y0-y1]),
+                       np.linalg.norm([x0-x2, y0-y2]))
+
+        return dist
 
     # 计算到线1的距离
-    if x11 <= x0 <= x12 and y11 <= y0 <= y12:
-        dist1 = 0
-    else:
-        # 到两端点的最小距离
-        dist1 = min(np.linalg.norm([x0-x11, y0-y11]),
-                    np.linalg.norm([x0-x12, y0-y12]))
-
+    dist1 = point_line_dist(x11, y11, x12, y12)
     # 计算到线2的距离
-    if x21 <= x0 <= x22 and y21 <= y0 <= y22:
-        dist2 = 0
-    else:
-        # 到两端点的最小距离
-        dist2 = min(np.linalg.norm([x0-x21, y0-y21]),
-                    np.linalg.norm([x0-x22, y0-y22]))
-
+    dist2 = point_line_dist(x21, y21, x22, y22)
     return dist1 + dist2
+
+
+if __name__ == '__main__':
+    pass
