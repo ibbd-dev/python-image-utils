4ff0a51ddfe3efe0248f193e55772982a234afe0
alex <cyy0523xc@gmail.com>
2020-11-03 17:02:04 +0800

    add video

--- tests/test_blur.py
+++ tests/test_blur.py
@@ -5,14 +5,49 @@ Author: alex
 Created Time: 2020年11月03日 星期二 10时03分21秒
 '''
 import shutil
+import imutils
 import cv2
+import skvideo.io
+import numpy as np
+from PIL import Image
 from image_utils.cv2_utils import lapulase
+from image_utils.convert import pil_cv2, cv2_pil
+
+
+def get_video_rotate(video_path):
+    """"""""
+    # video表示视频路径，字符串形式，eg：‘E:/project/blink-detection/3.mp4’
+    metadata = skvideo.io.ffprobe(video_path)
+    d = metadata['video'].get('tag')[0]
+    if d.setdefault('@key') == 'rotate': #获取视频自选择角度
+        return 360-int(d.setdefault('@value'))
+    return 0
+
+
+def rot90(img):
+    return img.swapaxes(0,1)[...,::-1]
+
+
+def rotate(img, angle):
+    img = cv2_pil(img)
+    if angle == 90:
+        img = img.transpose(Image.ROTATE_90)
+    elif angle == 270: 
+        img = img.transpose(Image.ROTATE_270)
+    elif angle == 180:
+        img = img.transpose(Image.ROTATE_180)
+    else:
+        raise Exception('Error')
+    return pil_cv2(img)
 
 
 def video_blur(video_path):
     vid = cv2.VideoCapture(video_path)
     if not vid.isOpened():
         raise IOError("Couldn't open webcam or video")
+    # 获取旋转角度
+    angle = get_video_rotate(video_path)
+    print('Angle: ', angle)
     # video_FourCC = int(vid.get(cv2.CAP_PROP_FOURCC))
     video_FourCC = cv2.VideoWriter_fourcc(*'mp4v')
     video_fps    = vid.get(cv2.CAP_PROP_FPS)
@@ -20,6 +55,9 @@ def video_blur(video_path):
                         int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT)))
 
     output_path = 'out_%s' % video_path.split('/')[-1]
+    if angle in {90, 270}:
+        video_size = video_size[::-1]
+
     print("!!! :", output_path, video_FourCC, video_fps, video_size)
     out = cv2.VideoWriter(output_path, video_FourCC, video_fps, video_size)
     blur_scores = []
@@ -28,6 +66,11 @@ def video_blur(video_path):
         return_value, frame = vid.read()
         if not return_value:
             break
+        if angle != 0:
+            # print(frame.shape, angle)
+            frame = rotate(frame, angle)
+            # print(frame.shape, angle)
+
         result = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         score = lapulase(result)
         text = 'Blur: %.2f' % score
